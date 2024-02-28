041c32598eb2099030b1bd5ce1b7d4dd533155e2
jusiam <mossakowska.justyna@gmail.com>
2024-02-28 16:33:57 +0200

    5

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
+#include <ctype.h>
 
 int main(void)
 {
     char alphabet;
 for (alphabet = 'a'; alphabet <= 'z' ; alphabet++)
 {
+    if (alphabet != 'e' && alphabet != 'q')
     putchar(alphabet);
     }
-putchar('\n');
+    putchar('\n');
+
+
 return (0);
 }
---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+    char digits;
+for (digits = '0'; digits <= '9' ; digits++)
+{
+    putchar(digits);
+    }
+putchar('\n');
+return (0);
+}
