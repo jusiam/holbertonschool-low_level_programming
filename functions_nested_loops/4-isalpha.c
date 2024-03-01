88cb77bff51d57143a4f0ee20330a9765af5422a
jusiam <mossakowska.justyna@gmail.com>
2024-03-01 19:34:10 +0200

    4

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,12 +1,14 @@
 #include "main.h"
 /**
- * Prototype: int _isalpha(int c);
+ * Prototype: int _isalpha int c
  * Returns 1 if c is a letter, lowercase or uppercase
  * Returns 0 otherwise
  */
 int _isalpha(int c)
 {
-if (c > 96 && c < 123 || c > 64 && c < 91)
+if (c > 96 && c < 123)
+return (1);
+if (c > 64 && c < 91)
 return (1);
 else
 return (0);
