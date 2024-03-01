fdc4c549ee7bdd6d560780e9af72826c10ba0ceb
jusiam <mossakowska.justyna@gmail.com>
2024-03-01 17:53:30 +0200

    1

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,15 +1,11 @@
 #include <unistd.h>
 #include "main.h"
-
 /**
- * main - Check description
- * Description: Write a function that prints the alphabet.
- * Return: 0.
+ *  main -Entry point
+ *
+ *  Return: Always 0 (Success)
  */
 void print_alphabet(void)
-/**
- * print_alphabet.
- */
 {
 char i;
 for (i = 'a'; i <= 'z'; i++)
