da22da16cd4c80a91a090fe729278e8070f3034f
jusiam <mossakowska.justyna@gmail.com>
2024-03-01 18:11:55 +0200

    2

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -8,7 +8,11 @@
 void print_alphabet_x10(void)
 {
 char i;
+int j;
+for (j = 0; j < 260; j++)
+{
 for (i = 'a'; i <= 'z'; i++)
-_putchar(i, i, i, i, i, i, i, i, i, i);
+_putchar(i);
 _putchar('\n');
+}
 }
