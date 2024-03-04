30c469517b73e6b8c668a12238a40451412346ac
jusiam <mossakowska.justyna@gmail.com>
2024-03-04 11:48:56 +0200

    8

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -13,17 +13,10 @@ void jack_bauer(void)
 	for (x = 0; x < 24; x++)
 	{
 		for (y = 0; y < 60; y++)
-		{
-			if (x < 24)
-			{
 			_putchar(x)
-			}
 			_putchar(':');
-			if (y < 60)
-			{
-				_putchar(y);
-			}
+			_putchar(y);
 			_putchar('\n');
 		}
 	}
-}
+	
