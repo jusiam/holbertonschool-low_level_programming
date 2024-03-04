7a587eb39ddabf9f08806e5f646ecb631e92ebed
jusiam <mossakowska.justyna@gmail.com>
2024-03-04 14:15:14 +0200

    5

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * more_numbers - check the code.
+ *
+ * Return: 0
+ */
+
+{
+	void more_numbers(void);
+    int x, y
+
+	for (x = 0; x <= 9; x++)
+	{
+		for (y = 0; y <= 14; y++)
+				_putchar(y)
+		_putchar('\n');
+	}
+}
