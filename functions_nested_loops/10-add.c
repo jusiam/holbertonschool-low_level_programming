af9125b23ee471d0a6320bead82f499bdb4a533b
jusiam <mossakowska.justyna@gmail.com>
2024-03-04 12:00:53 +0200

    91011

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * add - check the code
+ * @a: variable
+ * @b: variable
+ * Return: result.
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_to_98 - prints all natural numbers from n to 98
+ * @n: initial number
+ * Return: Always 0 (Success)
+ */
+
+void print_to_98(int n)
+{
+	while (n != 98)
+	{
+		if (n > 98)
+		{
+			printf("%d, ", n);
+			n--;
+		}
+		else
+		{
+			printf("%d, ", n);
+			n++;
+		}
+	}
+	printf("%d\n", n);
+}
