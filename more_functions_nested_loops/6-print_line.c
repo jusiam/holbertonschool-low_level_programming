339c04b63e8467e6d93cfa1fcbdabd0312268234
jusiam <mossakowska.justyna@gmail.com>
2024-03-04 15:53:48 +0200

    6

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_line - check the code.
+ * Return: returns 0
+ */
+
+void print_line(int n)
+{
+if (n <= 0)
+_putchar('\n');
+else
+for (n = 0 ; n ; n++)
+{
+_putchar("_");
+_putchar('\n');
+}
+}
