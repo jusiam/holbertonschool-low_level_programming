371ffe0973db6cf23edecdba124ab78bc00bccf3
jusiam <mossakowska.justyna@gmail.com>
2024-03-04 11:47:18 +0200

    8

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _abs - Computes the absolute value of an integer.
+ * @n: The integer to be computed.
+ *
+ * Return: The absolute value of the integer.
+ */
+void jack_bauer(void)
+{
+	int x, y;
+
+	for (x = 0; x < 24; x++)
+	{
+		for (y = 0; y < 60; y++)
+		{
+			if (x < 24)
+			{
+			_putchar(x)
+			}
+			_putchar(':');
+			if (y < 60)
+			{
+				_putchar(y);
+			}
+			_putchar('\n');
+		}
+	}
+}
