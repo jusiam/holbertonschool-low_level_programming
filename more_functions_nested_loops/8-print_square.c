e8cf8edca035c4d6db66dfe2b9e14e7e7dacde78
jusiam <mossakowska.justyna@gmail.com>
2024-03-04 17:00:41 +0200

    8

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -14,9 +14,9 @@ void print_square(int size)
 	{
 		int x, y;
 
-		for (x = 0 ; x < size ; x++)
+		for (x = 1 ; x < size ; x++)
 		{
-			for (y = 0 ; y <= x; y++)
+			for (y = 1 ; y <= x; y++)
 				_putchar('#');
 			_putchar('#');
 			_putchar('\n');
