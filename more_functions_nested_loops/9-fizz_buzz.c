20da11c83bef4e7bd958075630d705a552355771
jusiam <mossakowska.justyna@gmail.com>
2024-03-05 13:10:13 +0200

    9

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -13,13 +13,13 @@ int a;
 	for (a = 0; a <= 100; a++)
 {
 	if (a % 15 == 0)
-	printf(FizzBuzz);
+	printf("FizzBuzz");
 
 	else if (a % 3 == 0)
-	printf(Fizz);
+	printf("Fizz");
 
 	else if (a % 5 == 0)
-	printf(Buzz);
+	printf("Buzz");
 
 	else 
 	printf(%d, a);
