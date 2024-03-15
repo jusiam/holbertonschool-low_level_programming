a5d222ce6636b87013dcfc561a38dcf70df8dc79
jusiam <mossakowska.justyna@gmail.com>
2024-03-15 12:22:19 +0200

    012

---
+++ argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - check the code
+ * @argc: argc
+ * @argv: argv
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+    int i
+	printf("%d\n", argc(i));
+    i++;
+	return (0);
+}
---
+++ argc_argv/2-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - check the code
+ * @argc: argc
+ * @argv: argv
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", *argv);
+	return (0);
+}
