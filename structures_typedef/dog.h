6785011ecbab2901635da14bdcf78c1900907502
jusiam <mossakowska.justyna@gmail.com>
2024-04-12 13:16:43 +0300

    0

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,11 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
+
 /**
  * struct dog - struture of a dog
  * @name: name of dog
  * @age: age of dog
  * @owner: owner of dog
  */
+
 typedef struct dog
 {
 	char *name;
@@ -13,4 +15,10 @@ typedef struct dog
 	char *owner;
 
 }
-dog;
+dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif
